a7135799f5aca756ca9c2ca3f4165f41bf916883
SabaliDev <127083776+SabaliDev@users.noreply.github.com>
2024-10-21 16:28:43 +0300

    .

---
+++ src/to_do/traits/delete.rs
@@ -0,0 +1,6 @@
+pub trait Delete{
+    fn delete(&self, title:&str){
+        println!("{} is being deleted", title);
+    }
+   
+}
---
+++ src/to_do/traits/get.rs
@@ -0,0 +1,14 @@
+pub trait Get {
+    fn get(&self,title:&str){
+        print!("{} is being fetched",title)
+    }
+    
+}
+
+
+
+
+
+
+
+
